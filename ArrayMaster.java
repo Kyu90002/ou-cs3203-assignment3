4cc8db1dd8fa8a1a2ab84840ff3e7dc3852896d6
Kyumin Lee <2km4890@gmail.com>
2023-09-07 12:51:22 -0500

    Indentation fix.

--- ArrayMaster.java
+++ ArrayMaster.java
@@ -1,25 +1,23 @@
 public class ArrayMaster {
-    
-   public static int Sum_Is(int[] userArr) {
-       int sum = 0;
-      
-       for(int i = 0; i < userArr.length; i++) {
-           sum += userArr[i];
-       }
+	
+	public static int Sum_Is(int[] userArr) {
+		int sum = 0;
 
+		for(int i = 0; i < userArr.length; i++) {
+			sum += userArr[i];
+		}
 
-       return sum;
-   }
+		return sum;
+	}
 
-   public static int Product_Is(int[] userArr) {
-       int product = 1;
-      
-       for(int i = 0; i < userArr.length; i++) {
-           product *= userArr[i];
-       }
+	public static int Product_Is(int[] userArr) {
+		int product = 1;
 
+		for(int i = 0; i < userArr.length; i++) {
+			product *= userArr[i];
+		}
 
-       return product;
-   }
+		return product;
+	}
 
 }

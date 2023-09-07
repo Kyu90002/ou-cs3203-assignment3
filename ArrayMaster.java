162d6481cc2d01e6867db8676f49346a8b23243a
Kyumin Lee <2km4890@gmail.com>
2023-09-07 12:55:49 -0500

    Created main method.

--- ArrayMaster.java
+++ ArrayMaster.java
@@ -1,4 +1,11 @@
 public class ArrayMaster {
+
+	public static void main(String[] args) {
+		int[] userArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+		System.out.println(Sum_Is(userArr));
+		System.out.println(Product_Is(userArr));
+	}
 	
 	public static int Sum_Is(int[] userArr) {
 		int sum = 0;

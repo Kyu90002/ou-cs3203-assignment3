c1b9799f6ca47b8801ae1527d49ca7f4f9363125
Kyumin Lee <2km4890@gmail.com>
2023-09-07 12:29:04 -0500

    created ArrayMaster class and Sum_Is method.

--- ArrayMaster.java
+++ ArrayMaster.java
@@ -1,1 +1,14 @@
+public class ArrayMaster {
 
+
+   public static int Sum_Is(int[] userArr) {
+       int sum = 0;
+      
+       for(int i = 0; i < userArr.length; i++) {
+           sum += userArr[i];
+       }
+
+
+       return sum;
+   }
+}

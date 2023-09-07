1abcabc72dbe00d0bf848097d3aa032bab95e2a5
Kyumin Lee <2km4890@gmail.com>
2023-09-07 12:57:06 -0500

    Edited method names.

--- ArrayMaster.java
+++ ArrayMaster.java
@@ -3,11 +3,11 @@ public class ArrayMaster {
 	public static void main(String[] args) {
 		int[] userArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-		System.out.println(Sum_Is(userArr));
-		System.out.println(Product_Is(userArr));
+		System.out.println(sum_Is(userArr));
+		System.out.println(product_Is(userArr));
 	}
 	
-	public static int Sum_Is(int[] userArr) {
+	public static int sum_Is(int[] userArr) {
 		int sum = 0;
 
 		for(int i = 0; i < userArr.length; i++) {
@@ -17,7 +17,7 @@ public class ArrayMaster {
 		return sum;
 	}
 
-	public static int Product_Is(int[] userArr) {
+	public static int product_Is(int[] userArr) {
 		int product = 1;
 
 		for(int i = 0; i < userArr.length; i++) {
